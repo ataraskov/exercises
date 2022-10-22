4d661f063fbf7ed0136766d05b248cfbf92cadba
Alexey Taraskov <alexey.taraskov@gmail.com>
2022-10-22 12:34:04 +0300

    Add more tests for arrays and slices

--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -15,15 +15,36 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d, given %v", got, want, numbers)
 		}
 	})
+
+	t.Run("empty slice", func(t *testing.T) {
+		var numbers []int
+		got := Sum(numbers)
+		want := 0
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
 }
 
 func TestSumAll(t *testing.T) {
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
+	t.Run("empty slice", func(t *testing.T) {
+		got := SumAll(nil, nil)
+		want := []int{0, 0}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("non-empty slice", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
 
 func TestSumAllTails(t *testing.T) {
