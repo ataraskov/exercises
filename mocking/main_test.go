a008b7b5e9bfc0e6533dbdf22ab557634f3df0c9
Alexey Taraskov <alexey.taraskov@gmail.com>
2022-10-31 16:03:18 +0300

    Add mocking

---
+++ mocking/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const sleep = "sleep"
+const write = "write"
+
+func TestCounddown(t *testing.T) {
+	t.Run("output", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("sleep between prints ", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v, got %v", want, spySleepPrinter)
+		}
+	})
+}
