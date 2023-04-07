07b1fc8dee172c961fefc56e1c115ca5ad1bbad0
Alexey Taraskov <alexey.taraskov@gmail.com>
2023-04-07 11:52:09 +0300

    Update go:blackjack

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -9,36 +9,23 @@ const (
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	switch card {
-	case "ace":
-		return 11
-	case "jack":
-		return 10
-	case "queen":
-		return 10
-	case "king":
-		return 10
-	case "ten":
-		return 10
-	case "nine":
-		return 9
-	case "eight":
-		return 8
-	case "seven":
-		return 7
-	case "six":
-		return 6
-	case "five":
-		return 5
-	case "four":
-		return 4
-	case "three":
-		return 3
-	case "two":
-		return 2
-	default:
-		return 0
+	cards := map[string]int{
+		"ace":   11,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"ten":   10,
+		"jack":  10,
+		"queen": 10,
+		"king":  10,
 	}
+
+	return cards[card]
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
