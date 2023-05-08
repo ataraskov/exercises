99c63840d307ecdbffcf78455727d2ba9eda73a9
Alexey Taraskov <alexey.taraskov@gmail.com>
2023-05-08 09:29:16 +0200

    Update go:logs-logs-logs

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,34 +1,31 @@
 package logs
 
-import "unicode/utf8"
+import (
+	"strings"
+	"unicode/utf8"
+)
 
 // Application identifies the application emitting the given log.
 func Application(log string) string {
+	apps := map[rune]string{
+		'❗': "recommendation", // '\u2757'
+		'🔍': "search",         // '\u0001f50d'
+		'☀': "weather",        // '\u2600'
+	}
+
 	for _, r := range log {
-		switch {
-		case r == '\u2757':
-			return "recommendation" // ❗
-		case r == '🔍':
-			return "search" // 🔍 , \u1F50D gives error
-		case r == '\u2600':
-			return "weather" // ☀
+		if app, ok := apps[r]; ok {
+			return app
 		}
 	}
+
 	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	var result []byte
-	for _, r := range log {
-		if r == oldRune {
-			result = utf8.AppendRune(result, newRune)
-		} else {
-			result = utf8.AppendRune(result, r)
-		}
-	}
-	return string(result)
+	return strings.ReplaceAll(log, string(oldRune), string(newRune))
 }
 
 // WithinLimit determines whether or not the number of characters in log is
