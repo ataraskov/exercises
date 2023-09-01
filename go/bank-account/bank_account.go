385bbe0368e8eb19e4966a7d8400dc5032346c16
Alexey Taraskov <alexey.taraskov@gmail.com>
2023-09-01 12:59:19 +0200

    Add go:bank-account (WIP)

---
+++ go/bank-account/bank_account.go
@@ -0,0 +1,43 @@
+package account
+
+import "sync"
+
+type Account struct {
+	mu      sync.Mutex
+	closed  bool
+	balance int64
+}
+
+func Open(amount int64) *Account {
+	if amount < 0 {
+		return nil
+	}
+	return &Account{balance: amount}
+}
+
+func (a *Account) Balance() (int64, bool) {
+	if a.closed {
+		return 0, false
+	}
+	return a.balance, true
+}
+
+func (a *Account) Deposit(amount int64) (int64, bool) {
+	if a.closed {
+		return 0, false
+	}
+	if amount < 0 && amount*-1 > a.balance {
+		return 0, false
+	}
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	a.balance += amount
+	return a.balance, true
+}
+
+func (a *Account) Close() (int64, bool) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	a.closed = true
+	return a.balance, true
+}
