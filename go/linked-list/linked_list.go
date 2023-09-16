c8057f75a1407964e38b5a02c675423d74a1414c
Alexey Taraskov <alexey.taraskov@gmail.com>
2023-09-16 10:54:42 +0200

    Update go:linked-list

--- go/linked-list/linked_list.go
+++ go/linked-list/linked_list.go
@@ -1,45 +1,40 @@
+// Pacakge linkedlist provides a solution for exercise Linked List on Exercism's Go Track.
 package linkedlist
 
 import "fmt"
 
-// Define List and Node types here.
+// Node represents a node of linked list.
 type Node struct {
 	Value interface{}
 	prev  *Node
 	next  *Node
 }
 
+// List represents a linked list containing Nodes.
 type List struct {
 	head *Node
 }
 
+// NewList creates a new linked list from givin elements.
 func NewList(elements ...interface{}) *List {
 	l := List{}
-	var prev *Node
-	for i, e := range elements {
-		n := Node{
-			Value: e,
-			prev:  prev,
-		}
-		if prev != nil {
-			prev.next = &n
-		}
-		prev = &n
-		if i == 0 {
-			l.head = &n
-		}
+	for _, e := range elements {
+		l.Push(e)
 	}
 	return &l
 }
 
+// Next returns a pointer to the next node.
 func (n *Node) Next() *Node {
 	return n.next
 }
 
+// Prev returns a pointer to the previous node.
 func (n *Node) Prev() *Node {
 	return n.prev
 }
 
+// Unshift inserts a value at the front of the list.
 func (l *List) Unshift(v interface{}) {
 	n := &Node{
 		Value: v,
@@ -51,6 +46,7 @@ func (l *List) Unshift(v interface{}) {
 	l.head = n
 }
 
+// Push inserts a value at the back of the list.
 func (l *List) Push(v interface{}) {
 	n := &Node{
 		Value: v,
@@ -62,9 +58,9 @@ func (l *List) Push(v interface{}) {
 		l.head = n
 	}
 	n.prev = last
-
 }
 
+// Shift removes the head node from the list.
 func (l *List) Shift() (interface{}, error) {
 	if l.head != nil {
 		old := l.head
@@ -77,21 +73,23 @@ func (l *List) Shift() (interface{}, error) {
 	return nil, fmt.Errorf("empty list")
 }
 
+// Pop removes last node from the list.
 func (l *List) Pop() (interface{}, error) {
 	last := l.Last()
-	if last == nil {
+
+	switch last {
+	case nil:
 		return nil, fmt.Errorf("empty list")
-	}
-	if last == l.head {
+	case l.head:
 		l.head = nil
+	default:
+		last.prev.next = nil
 	}
-	if last.prev != nil {
-		newlast := last.prev
-		newlast.next = nil
-	}
+
 	return last.Value, nil
 }
 
+// Reverse updates node lins to reverse the list.
 func (l *List) Reverse() {
 	reversed := NewList()
 	n := l.head
@@ -102,10 +100,12 @@ func (l *List) Reverse() {
 	l.head = reversed.head
 }
 
+// First returns a pointer to the head node of the list.
 func (l *List) First() *Node {
 	return l.head
 }
 
+// First returns a pointer to the last node of the list.
 func (l *List) Last() *Node {
 	n := l.head
 	for n != nil && n.next != nil {
