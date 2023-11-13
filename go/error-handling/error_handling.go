97843b1804eef79e503e0ec3e9b86e50a491174a
Alexey Taraskov <alexey.taraskov@gmail.com>
2023-11-13 19:21:55 +0100

    Update go:error_handling

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -4,9 +4,8 @@ package erratum
 import "fmt"
 
 // Use an opener on provided input
-func Use(opener ResourceOpener, input string) error {
+func Use(opener ResourceOpener, input string) (err error) {
 	var res Resource
-	var err error
 
 	for ok := true; ok; _, ok = err.(TransientError) {
 		res, err = opener()
@@ -17,16 +16,15 @@ func Use(opener ResourceOpener, input string) error {
 	// close resource
 	defer func() {
 		closeErr := res.Close()
-		fmt.Println("closing res, err:", err, "closeErr:", closeErr)
 		if closeErr != nil {
 			err = closeErr
 		}
+		return
 	}()
 
 	// handle possible panic
 	defer func() {
 		r := recover()
-		fmt.Println("checking for panic, err:", r)
 		if r != nil {
 			if err, ok := r.(FrobError); ok {
 				fmt.Println("calling Defrob")
@@ -37,6 +35,5 @@ func Use(opener ResourceOpener, input string) error {
 	}()
 	res.Frob(input)
 
-	fmt.Println("end of func, err:", err)
 	return err
 }
