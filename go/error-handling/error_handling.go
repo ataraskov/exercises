aee3e10f97d4efcf9eea1f86b3a40380d2142bf9
Alexey Taraskov <alexey.taraskov@gmail.com>
2023-11-16 10:28:47 +0100

    Update go:error_handling

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,33 +1,24 @@
 // Package erratum is a solution for exercise Error Handling on Exercism's Go Track.
 package erratum
 
-import "fmt"
-
 // Use an opener on provided input
 func Use(opener ResourceOpener, input string) (err error) {
 	var res Resource
 
+	// open resource
 	for ok := true; ok; _, ok = err.(TransientError) {
 		res, err = opener()
+		// there should be some limiter
 	}
 	if err != nil {
 		return err
 	}
-	// close resource
-	defer func() {
-		closeErr := res.Close()
-		if closeErr != nil {
-			err = closeErr
-		}
-		return
-	}()
+	defer res.Close()
 
 	// handle possible panic
 	defer func() {
-		r := recover()
-		if r != nil {
+		if r := recover(); r != nil {
 			if err, ok := r.(FrobError); ok {
-				fmt.Println("calling Defrob")
 				res.Defrob(err.defrobTag)
 			}
 			err = r.(error)
