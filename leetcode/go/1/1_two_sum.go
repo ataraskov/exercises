a3ea479fed2a999210769197d2688a3809f6e82d
ataraskov <alexey.taraskov@gmail.com>
2024-03-05 18:50:43 +0100

    Update Two Sum

--- leetcode/go/1/1_two_sum.go
+++ leetcode/go/1/1_two_sum.go
@@ -7,12 +7,30 @@ package main
 // [1,1], 2     => 0,1
 // [1,0,0,1], 2 => 0,3
 func twoSum(nums []int, target int) []int {
-	for i := range nums {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
+	// brute force
+	// -------------------------------------------------------------------------
+	// complexity: O(n^2)
+	// space: O(1)
+	/*
+		for i := range nums {
+			for j := i + 1; j < len(nums); j++ {
+				if nums[i]+nums[j] == target {
+					return []int{i, j}
+				}
 			}
 		}
+	*/
+
+	// using map
+	// -------------------------------------------------------------------------
+	// complexity: O(n)
+	// space: O(n)
+	m := make(map[int]int)
+	for idx, num := range nums {
+		if pos, ok := m[target-num]; ok {
+			return []int{pos, idx}
+		}
+		m[num] = idx
 	}
 	return []int{}
 }
