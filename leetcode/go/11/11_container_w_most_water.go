04dad473c60dce4b0e33b5f7468122f7bc72973e
ataraskov <alexey.taraskov@gmail.com>
2024-03-07 16:05:37 +0100

    Update Container With Most Water

--- leetcode/go/11/11_container_w_most_water.go
+++ leetcode/go/11/11_container_w_most_water.go
@@ -12,11 +12,29 @@ func maxArea(height []int) int {
 	// -------------------------------------------------------------------------
 	// complexity: O(n^2)
 	// space: O(1)
+	/*
+		area := 0
+		for i := range height {
+			for j := i + 1; j < len(height); j++ {
+				a := min(height[i], height[j]) * (j - i)
+				area = max(area, a)
+			}
+		}
+	*/
+
+	// two pointers
+	// -------------------------------------------------------------------------
+	// complexity: O(n)
+	// space: O(1)
 	area := 0
-	for i := range height {
-		for j := i + 1; j < len(height); j++ {
-			a := min(height[i], height[j]) * (j - i)
-			area = max(area, a)
+	i, j := 0, len(height)-1
+	for i < j {
+		a := min(height[i], height[j]) * (j - i)
+		area = max(area, a)
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
 		}
 	}
 	return area
