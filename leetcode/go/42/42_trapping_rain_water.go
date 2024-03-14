15c5e366e9ea490c447d8c5e4f24d1a83fdfcc65
ataraskov <alexey.taraskov@gmail.com>
2024-03-14 20:49:37 +0100

    Update Trapping Rain Water

--- leetcode/go/42/42_trapping_rain_water.go
+++ leetcode/go/42/42_trapping_rain_water.go
@@ -7,15 +7,45 @@ func trap(height []int) int {
 	// -------------------------------------------------------------------------
 	// complexity: O(n^2)
 	// space: O(1)
+	/*
+		total := 0
+		for i, val := range height {
+			lMax := max(height[:i])
+			rMax := max(height[i+1:])
+			capacity := min(lMax, rMax) - val
+			if capacity > 0 {
+				total += capacity
+			}
+		}
+	*/
+
+	// two pointers
+	// -------------------------------------------------------------------------
+	// complexity: O(n)
+	// space: O(1)
 	total := 0
-	for i, val := range height {
-		lMax := max(height[:i])
-		rMax := max(height[i+1:])
-		capacity := min(lMax, rMax) - val
-		if capacity > 0 {
-			total += capacity
+	l, r := 0, len(height)-1
+	lMax, rMax := 0, 0
+
+	for l < r {
+		lHeight, rHeight := height[l], height[r]
+		if lHeight < rHeight {
+			if lHeight > lMax {
+				lMax = lHeight
+			} else {
+				total += lMax - lHeight
+			}
+			l++
+		} else {
+			if rHeight > rMax {
+				rMax = rHeight
+			} else {
+				total += rMax - rHeight
+			}
+			r--
 		}
 	}
+
 	return total
 }
 
