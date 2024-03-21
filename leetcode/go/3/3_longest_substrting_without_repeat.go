5a76a0b579596411f733d8e89eaaf21e58a716c8
ataraskov <alexey.taraskov@gmail.com>
2024-03-21 16:19:40 +0100

    Update Longest Substring Without Repeat

--- leetcode/go/3/3_longest_substrting_without_repeat.go
+++ leetcode/go/3/3_longest_substrting_without_repeat.go
@@ -9,16 +9,16 @@ func lengthOfLongestSubstring(s string) int {
 	// space: O(n)
 	n := 0
 	for i := 0; i < len(s); i++ {
-		seen := make(map[rune]int)
+		seen := make(map[rune]bool)
 		for j := i; j < len(s); j++ {
 			r := []rune(s)[j]
-			if _, found := seen[r]; found {
+			if seen[r] {
 				break
 			}
 			if j-i+1 > n {
 				n = j - i + 1
 			}
-			seen[r] = 1
+			seen[r] = true
 		}
 	}
 	return n
