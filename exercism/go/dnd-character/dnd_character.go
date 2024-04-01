497c72362f12a7c49a82cb6b368b80d15f629705
ataraskov <alexey.taraskov@gmail.com>
2024-04-01 14:55:17 +0200

    Add dnd_character

---
+++ exercism/go/dnd-character/dnd_character.go
@@ -0,0 +1,45 @@
+package dndcharacter
+
+import (
+	"math"
+	"math/rand"
+)
+
+type Character struct {
+	Strength     int
+	Dexterity    int
+	Constitution int
+	Intelligence int
+	Wisdom       int
+	Charisma     int
+	Hitpoints    int
+}
+
+// Modifier calculates the ability modifier for a given ability score
+func Modifier(score int) int {
+	n := float64(score-10) / 2
+	if n < 0 {
+		return int(math.Floor(n))
+	}
+	return int(n)
+}
+
+// Ability uses randomness to generate the score for an ability
+func Ability() int {
+	return rand.Intn(6) + rand.Intn(6) + rand.Intn(6) + 3
+}
+
+// GenerateCharacter creates a new Character with random scores for abilities
+func GenerateCharacter() Character {
+	c := Character{
+		Strength:     Ability(),
+		Dexterity:    Ability(),
+		Constitution: Ability(),
+		Intelligence: Ability(),
+		Wisdom:       Ability(),
+		Charisma:     Ability(),
+		Hitpoints:    10,
+	}
+	c.Hitpoints += Modifier(c.Constitution)
+	return c
+}
