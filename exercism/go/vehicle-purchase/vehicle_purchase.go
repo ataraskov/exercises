a9e4634dd3f55fb6d61ae2ff1603e6e9aed31e76
ataraskov <alexey.taraskov@gmail.com>
2024-06-28 11:06:30 +0200

    Update vehicle-purchase

--- exercism/go/vehicle-purchase/vehicle_purchase.go
+++ exercism/go/vehicle-purchase/vehicle_purchase.go
@@ -4,10 +4,7 @@ const choice_pharse = " is clearly the better choice."
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	if kind == "car" || kind == "truck" {
-		return true
-	}
-	return false
+	return kind == "car" || kind == "truck"
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
